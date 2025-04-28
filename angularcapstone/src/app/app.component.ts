f6c4b2473a79a4f65a43495f14db35b6be7999ae
James Lawrence <jamesblawrence2007@gmail.com>
2025-04-28 10:03:25 -0600

    fixed bugs that broke the ability to route to the dashboard page on login. dashboard can now be accessed after login but nothing displays. will fix that tomorrow

--- angularcapstone/src/app/app.component.ts
+++ angularcapstone/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,19 +11,23 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: firebase.User | null = null;
-  constructor(private afAuth: AngularFireAuth, private router: Router) {
+
+  constructor(private afAuth: AngularFireAuth, private router: Router) { // Inject Router here
     this.afAuth.authState.subscribe((user) => {
+      console.log('Auth State:', user);
       this.user = user;
       if (user) {
-        this.router.navigate(['/dashboard']);
+        this.router.navigate(['/dashboard']); // Use the injected Router service
       }
     });
   }
+
   login() {
     this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
+
   logout() {
     this.afAuth.signOut();
-    this.router.navigate(['/']);
+    this.router.navigate(['/']); // Use the injected Router service
   }
 }
--- angularcapstone/src/app/app.module.ts
+++ angularcapstone/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from '../environment/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
 @NgModule({
   declarations: [
-    DashboardComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
